e1ffa18297ee2b63f246e9c530b4da09af4a655b
JanaEsther <j.obendraufova@h2o-logic.cz>
2024-10-17 13:49:55 +0200

    setGlowing

--- src/components/Star/index.jsx
+++ src/components/Star/index.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react';
 import './style.css';
 
-export const Star = () => {
+export const Star = ({value, onSelect}) => {
   const [glowing, setGlowing] = useState(false);
-  
-  const starClass = glowing
-    ? 'rating__star rating__star--on'
-    : 'rating__star';
 
-  return (
-    <div className={starClass}></div>
-  );
+
+
+  const handleStar = () => {
+    setGlowing(true);
+  };
+
+  const starClass = glowing ? 'rating__star rating__star--on' : 'rating__star';
+
+  return <div className={starClass} 
+  onClick={handleStar}
+  ></div>;
 };
