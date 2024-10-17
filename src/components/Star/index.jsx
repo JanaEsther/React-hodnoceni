2ef9152efedbcb3ada8f5f415a04de4e1ced1f29
JanaEsther <j.obendraufova@h2o-logic.cz>
2024-10-17 14:51:04 +0200

    uprava-bez stavu glowing

--- src/components/Star/index.jsx
+++ src/components/Star/index.jsx
@@ -1,22 +1,15 @@
 import { useState } from 'react';
 import './style.css';
 
-export const Star = ({ value, onSelect }) => {
-  const [glowing, setGlowing] = useState(false);
+export const Star = ({ value, glowing, onSelect }) => {
+  //const [glowing, setGlowing] = useState(false);
 
   const handleStar = () => {
-    setGlowing(true)
-    onSelect(value)
-    ;
+    glowing === true;
+    onSelect(value); // Call onSelect with the star value
   };
 
   const starClass = glowing ? 'rating__star rating__star--on' : 'rating__star';
 
-  return (
-    <div
-      className={starClass}
-      onClick={handleStar}
-      value={value}
-    ></div>
-  );
+  return <div className={starClass} onClick={handleStar}></div>;
 };
